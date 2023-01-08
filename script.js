4b8b34a5f1ac9ecd8a4c070635a9a2026e96c340
GitsOfVivek <singhvivek1999@yahoo.com>
2023-01-08 11:16:26 +0530

    challenge 2 using arrow function

--- script.js
+++ script.js
@@ -228,12 +228,11 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
 
-// const calcAverageHumanAge = function (arr) {
-//   return arr
+// const calcAverageHumanAge = arr =>
+//   arr
 //     .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
 //     .filter(age => age >= 18)
 //     .reduce((sum, cur, _, arr) => sum + cur / arr.length, 0);
-// };
 // console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
 // console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
 
